a08912015666ea85223099d4f317e29fbe55b9f3
TAGNE-MOGUE <lauramogue7@gmail.com>
2026-05-11 01:35:58 +0200

    Ajout du TP1 IPPlan-Manager-Decouverte

---
+++ Projet1POO/TP1_IPPlanManager_Decouverte/src/IPPlanmanager/AdresseIP.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IPPlanmanager;
+
+/**
+ *
+ * @author GENERAL STORES
+ */
+public class AdresseIP {
+    String valeur;
+
+    public AdresseIP(String valeur) {
+        this.valeur = valeur;
+    }
+
+    public void afficher() {
+        System.out.println("Adresse IP : " + valeur);
+    }
+
+}
+
+    
+
